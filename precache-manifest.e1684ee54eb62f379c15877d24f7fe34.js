dd7f3baad430102e503ff6bdd75a1d954ec6dd46
RaulCote <raulcote@hotmail.com>
2018-12-19 16:32:30 +0100

    Updates

---
+++ precache-manifest.e1684ee54eb62f379c15877d24f7fe34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb44e6bead78593f11fd",
+    "url": "/users-management/static/js/runtime~main.fb44e6be.js"
+  },
+  {
+    "revision": "a6563f3fa40e11cbc821",
+    "url": "/users-management/static/js/main.a6563f3f.chunk.js"
+  },
+  {
+    "revision": "fc71c02cc86bc6d43b59",
+    "url": "/users-management/static/js/1.fc71c02c.chunk.js"
+  },
+  {
+    "revision": "a6563f3fa40e11cbc821",
+    "url": "/users-management/static/css/main.e26562af.chunk.css"
+  },
+  {
+    "revision": "729327fb8fdcbd83715fc706659b7032",
+    "url": "/users-management/index.html"
+  }
+];
